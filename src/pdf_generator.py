724a0e726b1c10d7e94229f64a7c17ed91d38957
AgustinMadygraf <agustin.mtto.madygraf>
2024-04-14 22:04:35 -0300

    Refactor PDF generation code in pdf_generator.py to include a banner image and improve layout

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -1,25 +1,32 @@
 #src/pdf_generator.py
-import os
-from reportlab.lib.pagesizes import letter
+from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
+from reportlab.lib.units import inch, mm
+import os
 
 def create_pdf(data, filename):
-    c = canvas.Canvas(filename, pagesize=letter)
-    width, height = letter  # Asume tamaño de carta
+    c = canvas.Canvas(filename, pagesize=A4)
+    width, height = A4  # Usar el tamaño de página A4
 
-    # Cargar y dibujar la imagen del banner
+    # Definir los márgenes
+    top_margin = 10 * mm  # Margen superior
+    side_margin = 10 * mm  # Margen lateral
+    banner_height = 30 * mm  # Altura del banner
+
+    # Cargar y dibujar la imagen del banner con márgenes
     banner_path = os.path.join(os.path.dirname(__file__), '..', 'resources', 'banner.jpg')
-    c.drawImage(banner_path, 0, height - inch, width, inch)  # Asumiendo que el banner tiene 1 pulgada de altura
+    c.drawImage(banner_path, side_margin, height - top_margin - banner_height, width - 2 * side_margin, banner_height, preserveAspectRatio=True, anchor='n')
 
     # Asegurarse de que las claves existen en el diccionario
     nombre_proyecto = data.get('nombre_proyecto', 'N/A') if data else 'N/A'
     presupuesto_total = data.get('presupuesto_total', 'N/A') if data else 'N/A'
     presupuesto_gastado = data.get('presupuesto_gastado', 'N/A') if data else 'N/A'
 
-    c.drawString(100, height - 1.5 * inch, f"Proyecto: {nombre_proyecto}")
-    c.drawString(100, height - 1.75 * inch, f"Presupuesto Total: ${presupuesto_total}")
-    c.drawString(100, height - 2 * inch, f"Presupuesto Gastado: ${presupuesto_gastado}")
+    text_start = height - top_margin - banner_height - 15 * mm  # Espacio debajo del banner
+
+    c.drawString(side_margin, text_start, f"Proyecto: {nombre_proyecto}")
+    c.drawString(side_margin, text_start - 15 * mm, f"Presupuesto Total: ${presupuesto_total}")
+    c.drawString(side_margin, text_start - 30 * mm, f"Presupuesto Gastado: ${presupuesto_gastado}")
 
     # Finaliza el PDF
     c.save()
