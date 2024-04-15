07e8c0535e6905438f39062d80bad39e5efb0e61
AgustinMadygraf <agustin.mtto.madygraf>
2024-04-14 23:14:33 -0300

    Refactor PDF generation code in pdf_generator.py to update sub banner text and improve layout

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -22,12 +22,26 @@ def create_pdf(data, filename):
     c.drawImage(banner_path, side_margin, height - top_margin - banner_height, width - 2 * side_margin, banner_height, preserveAspectRatio=True, anchor='n')
 
     # Texto debajo del banner
-    sub_banner_text_left = "Cooperativa de Trabajo MADYGRAF LTDA"
-    sub_banner_text_right = " Fecha 26/12/2023"
-    sub_banner_text_size = 12  # Tamaño de fuente para el texto debajo del banner
-    c.setFont("Helvetica", sub_banner_text_size)
-    c.drawString(side_margin, height - top_margin - banner_height - 15 * mm, sub_banner_text_left)
-    c.drawRightString(width - side_margin, height - top_margin - banner_height - 15 * mm, sub_banner_text_right)
+    sub_banner_text_left_1 = "Cooperativa de Trabajo MADYGRAF LTDA"
+    sub_banner_text_left_2 = "Ruta panamerica 36.700 - Garin - 1618"
+    sub_banner_text_left_3 = "C.U.I.T.: 33 71465177 9"
+    sub_banner_text_left_4 = "Teléfono: 11 4035-5771"
+    sub_banner_text_right_1_a = "Fecha"
+    sub_banner_text_right_1_b = "26/12/2023" # deberá ser dinámico
+    sub_banner_text_right_2_a = "N° de presupuesto"
+    sub_banner_text_right_2_b = "0001" # deberá ser dinámico
+    sub_banner_text_size = 9
+    c.setFont("Helvetica-Bold", sub_banner_text_size)  
+    c.drawString(side_margin, height - top_margin - banner_height - 5 * mm, sub_banner_text_left_1)
+    c.drawString(width - side_margin - 40 * mm, height - top_margin - banner_height - 5 * mm, sub_banner_text_right_1_a)
+    c.drawString(width - side_margin - 55 * mm, height - top_margin - banner_height - 10 * mm, sub_banner_text_right_2_a)
+    c.setFont("Helvetica", sub_banner_text_size)  
+    c.drawRightString(width - side_margin, height - top_margin - banner_height - 5 * mm, sub_banner_text_right_1_b)
+    c.drawString(side_margin, height - top_margin - banner_height - 10 * mm, sub_banner_text_left_2)
+    c.drawString(side_margin, height - top_margin - banner_height - 15 * mm, sub_banner_text_left_3)
+    c.drawString(side_margin, height - top_margin - banner_height - 20 * mm, sub_banner_text_left_4)
+    c.drawRightString(width - side_margin, height - top_margin - banner_height - 10 * mm, sub_banner_text_right_2_b)
+
 
 
     c.save()
