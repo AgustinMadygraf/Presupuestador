408ad19a9b27694c62c8a530c7bb88f4b95101ac
AgustinMadygraf <agustin.mtto.madygraf>
2024-04-18 16:56:58 -0300

    Refactor main.py to update budget creation process and import clients from CSV file

--- src/database.py
+++ src/database.py
@@ -9,10 +9,76 @@ logger = configurar_logging()
 
 load_dotenv()
 
+def create_connection():
+    """Create a database connection to the MySQL database"""
+    conn = None
+
+    host        = os.getenv('MYSQL_HOST')
+    user        = os.getenv('MYSQL_USER')
+    password    = os.getenv('MYSQL_PASSWORD')
+    db_name     = os.getenv('MYSQL_DB')
+    
+    # Asegúrate de que todas las variables de entorno estén configuradas
+    if None in (db_name, host, user, password):
+        logger.error("Falta una o más variables de entorno requeridas para la conexión a la base de datos.")
+        return None
+
+    try:
+        # Intenta conectarse a la base de datos especificada
+        conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name
+        )
+        if conn.is_connected():
+            db_info = conn.get_server_info()
+            logger.info(f"Conectado al servidor MySQL versión {db_info}")
+            create_tables(conn)
+    except Error as e:
+        if 'Unknown database' in str(e):
+            # Si la base de datos no existe, crea una conexión sin especificar la base de datos
+            conn = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password
+            )
+            if conn.is_connected():
+                logger.info("Conexión inicial sin base de datos establecida exitosamente.")
+                # Crea la base de datos
+                create_database(conn, db_name)
+                # Cierra la conexión inicial y reconéctate a la nueva base de datos
+                conn.close()
+                conn = mysql.connector.connect(
+                    host=host,
+                    user=user,
+                    password=password,
+                    database=db_name
+                )
+                logger.info(f"Reconectado a la nueva base de datos '{db_name}'.")
+                create_tables(conn)
+        else:
+            logger.error(f"Error al conectar a la base de datos: {e}", exc_info=True)
+    return conn
+
+def create_database(conn, db_name):
+    """Create the database if it does not exist and create tables."""
+    cursor = conn.cursor()
+    try:
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
+        conn.database = db_name  # Esto es necesario para apuntar a la base de datos recién creada
+        logger.info(f"Base de datos '{db_name}' creada exitosamente.")
+        create_tables(conn)  # Llamada a la función para crear tablas
+    except Error as e:
+        logger.error(f"Error al crear la base de datos: {e}", exc_info=True)
+    finally:
+        cursor.close()
+
 def create_tables(conn):
     """Create tables in the specified database."""
     cursor = conn.cursor()
     try:
+        logger.debug("Creando tablas en la base de datos")
         # Creación de la tabla 'presupuestos'
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS presupuestos (
@@ -46,14 +112,17 @@ def create_tables(conn):
         # Creación de la tabla 'clientes'
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS clientes (
-            ID_clientes INT AUTO_INCREMENT PRIMARY KEY,
+            ID_cliente INT AUTO_INCREMENT PRIMARY KEY,
             CUIT VARCHAR(255),
+            Razon_social VARCHAR(255),
+            Direccion VARCHAR(255),
+            Ubicacion_geografica VARCHAR(255),
+            N_contacto VARCHAR(255),
             nombre VARCHAR(255),
             apellido VARCHAR(255),
-            Razon_social VARCHAR(255),
-            N_contacto VARCHAR(255),
-            Direccion VARCHAR(255),
-            Ubicacion_geografica VARCHAR(255)
+            Unidad_de_negocio VARCHAR(255),
+            Legajo_vendedor INT,
+            Facturacion_anual FLOAT
         );
         """)
         logger.info("Tabla 'clientes' creada exitosamente.")
@@ -76,67 +145,8 @@ def create_tables(conn):
     finally:
         cursor.close()
 
-def create_database(conn, db_name):
-    """Create the database if it does not exist and create tables."""
-    cursor = conn.cursor()
-    try:
-        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name};")
-        conn.database = db_name  # Esto es necesario para apuntar a la base de datos recién creada
-        logger.info(f"Base de datos '{db_name}' creada exitosamente.")
-        create_tables(conn)  # Llamada a la función para crear tablas
-    except Error as e:
-        logger.error(f"Error al crear la base de datos: {e}", exc_info=True)
-    finally:
-        cursor.close()
 
-def create_connection():
-    """Create a database connection to the MySQL database"""
-    conn = None
 
-    host        = os.getenv('MYSQL_HOST')
-    user        = os.getenv('MYSQL_USER')
-    password    = os.getenv('MYSQL_PASSWORD')
-    db_name     = os.getenv('MYSQL_DB')
-    
-    # Asegúrate de que todas las variables de entorno estén configuradas
-    if None in (db_name, host, user, password):
-        logger.error("Falta una o más variables de entorno requeridas para la conexión a la base de datos.")
-        return None
 
-    try:
-        # Intenta conectarse a la base de datos especificada
-        conn = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name
-        )
-        if conn.is_connected():
-            db_info = conn.get_server_info()
-            logger.info(f"Conectado al servidor MySQL versión {db_info}")
-    except Error as e:
-        if 'Unknown database' in str(e):
-            # Si la base de datos no existe, crea una conexión sin especificar la base de datos
-            conn = mysql.connector.connect(
-                host=host,
-                user=user,
-                password=password
-            )
-            if conn.is_connected():
-                logger.info("Conexión inicial sin base de datos establecida exitosamente.")
-                # Crea la base de datos
-                create_database(conn, db_name)
-                # Cierra la conexión inicial y reconéctate a la nueva base de datos
-                conn.close()
-                conn = mysql.connector.connect(
-                    host=host,
-                    user=user,
-                    password=password,
-                    database=db_name
-                )
-                logger.info(f"Reconectado a la nueva base de datos '{db_name}'.")
-        else:
-            logger.error(f"Error al conectar a la base de datos: {e}", exc_info=True)
-    return conn
 
 
--- src/main.py
+++ src/main.py
@@ -21,59 +21,52 @@ def main_menu():
 
 def handle_new_presupuesto(conn):
     """
-    Crea un nuevo presupuesto en la base de datos y devuelve el ID del mismo.
-
+    Creates a new budget in the database and returns its ID.
     """
     cursor = conn.cursor() 
-    cursor.execute("SHOW TABLES LIKE 'presupuestos';")
-    if cursor.fetchone() is None:
-        print("No se encontró la tabla 'presupuestos'. Creando las tablas")
-        # Crear las tablas en la base de datos
-        create_tables(conn)
-        print("Tablas creadas exitosamente.")
 
+    # Check if the 'presupuestos' or 'clientes' table exists in the database
+    if not table_exists(cursor, 'presupuestos'):
+        print("The 'presupuestos' table was not found. Creating tables...")
+        create_tables(conn)
+        print("Tables created successfully.")
+
+    new_id = get_next_budget_id(cursor)
+    print(f"\nCreating a new budget with ID {new_id}\n")
+
+    # Insert the new budget into the database, with the corresponding ID. through inputs
+    clientes = get_all_clients(cursor)
+
+    # Create a boolean variable to identify whether I have a client list or not
+    have_clients = bool(clientes)
+
+    if have_clients:
+        print_client_list(clientes)
+    else:
+        print(Fore.RED + "There are no clients in the list.\n")
+        importar_clientes()
+
+def table_exists(cursor, table_name):
+    cursor.execute(f"SHOW TABLES LIKE '{table_name}';")
+    return cursor.fetchone() is not None
+
+def get_next_budget_id(cursor):
     cursor.execute("SELECT MAX(ID_presupuesto) FROM presupuestos;")
     max_id = cursor.fetchone()[0]
-    if max_id is None:
-        max_id = 0
-    new_id = max_id + 1
-    print("")
-    print(f"Creando un nuevo presupuesto con ID {new_id}\n")
-    # Insertar el nuevo presupuesto en la base de datos, con el ID correspondiente. por medio de inputs
+    return max_id + 1 if max_id is not None else 1
+
+def get_all_clients(cursor):
+    if not table_exists(cursor, 'clientes'):
+        print("The 'clientes' table does not exist. Please create it before proceeding.")
+        return []
     cursor.execute("SELECT * FROM clientes;")
-    clientes = cursor.fetchall()
-    # Crear una variable booleana para identificar si tengo o no tengo listado de clientes
-    tengo_clientes = bool(clientes)
+    return cursor.fetchall()
 
-    if tengo_clientes:
-        print("Lista de clientes:")
-        for cliente in clientes:
-            print(f"{cliente[0]}. {cliente[1]}")
-    else:
-        print(Fore.RED + "No hay clientes en la lista.\n")
-        importar_clientes()
+def print_client_list(clientes):
+    print("Client list:")
+    for cliente in clientes:
+        print(f"{cliente[0]}. {cliente[1]}")
     
-
-
-
-
-
-    # print("Lista de clientes:")
-    # for cliente in clientes:
-    #     print(f"{cliente[0]}. {cliente[1]}")
-    # cliente_id = input("ID del cliente o 'n' para agregar uno nuevo: ")
-    # if cliente_id == 'n':
-    #     nombre_cliente = input("Nombre del cliente: ")
-    #     cursor.execute("INSERT INTO clientes (nombre) VALUES (%s);", (nombre_cliente,))
-    #     conn.commit()
-    #     cliente_id = cursor.lastrowid
-    # else:
-    #     cliente_id = int(cliente_id)
-    #luego se ofrece una lista de los productos, para seleccionar uno. 
-
-
-    
-
 
 
 def get_presupuesto_id():
@@ -159,6 +152,9 @@ def importar_clientes():
         with open('database/clientes.csv', 'r') as file:
             logger.debug("Abriendo el archivo 'clientes.csv'")
             reader = csv.reader(file)
+            #abrir conexion con Mysql
+            conn = create_connection()
+            cursor = conn.cursor()
             try:
                 logger.debug("Leyendo la cabecera del archivo 'clientes.csv'")
                 row_cabecera = next(reader)
@@ -168,8 +164,12 @@ def importar_clientes():
                 row = next(reader)
                 logger.debug("Descartando la primera fila del archivo 'clientes.csv'")
                 logger.debug("Leyendo los clientes del archivo 'clientes.csv'")
-                logger.debug(f"{row_cabecera[0]}: {row[0]}, {row_cabecera[1]}: {row[1]}, {row_cabecera[2]}: {row[2]}, {row_cabecera[3]}: {row[3]}, {row_cabecera[4]}: {row[4]}, {row_cabecera[5]}: {row[5]} ")
-            
+                logger.debug(f"{row_cabecera[0]}: {row[0]}, {row_cabecera[1]}: {row[1]}, {row_cabecera[2]}: {row[2]}, {row_cabecera[3]}: {row[3]}, {row_cabecera[4]}: {row[4]}, {row_cabecera[5]}: {row[5]}, {row_cabecera[6]}: {row[6]}, {row_cabecera[7]}: {row[7]}")
+                logger.debug("Insertando los clientes en la base de datos")
+                sql = "INSERT INTO clientes (ID_cliente,CUIT,Razon_social,Direccion,Ubicacion_geografica,N_contacto,nombre,apellido,Unidad_de_negocio,Legajo_vendedor,Facturacion_anual) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);", (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])                
+                cursor.execute(sql)
+                conn.commit()
+                
             except StopIteration:
                 logger.warning("El archivo 'clientes.csv' está vacío.")
                 return clientes
@@ -177,7 +177,7 @@ def importar_clientes():
                 clientes.append(row)
     except FileNotFoundError:
         logger.error("No se pudo abrir el archivo 'clientes.csv'.")
-    except Exception as e:
+    except Exception as e: #mayor detalle de este error
         logger.error(f"Error al importar clientes desde el archivo 'clientes.csv': {e}")
     return clientes
 
