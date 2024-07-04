1d98b99d80f47456a3545f3d0f15dea1a146d8d3
AgustinMadygraf <agustin.mtto.madygraf>
2024-07-03 22:45:50 -0300

    Refactor file path in main.py

--- tests/test_budget_management.py
+++ tests/test_budget_management.py
@@ -50,36 +50,7 @@ class TestBudgetManager(unittest.TestCase):
         mock_print.assert_called_with("Error al insertar vendedor:", self.mock_cursor.execute.side_effect)
         self.mock_conn.rollback.assert_called_once()
 
-    @patch('src.client_selection.select_client', return_value=1)
-    @patch('src.database.get_new_budget_id', return_value=123)
-    @patch('src.models.budget_management.BudgetManager.select_salesperson', return_value=456)
-    @patch('src.client_selection.input_validado', return_value=30)
-    def test_collect_budget_data(self, mock_input_validado, mock_select_salesperson, mock_get_new_budget_id, mock_select_client):
-        with patch('builtins.input', side_effect=['S', '2024-07-03', 'Comentario', 'S']):
-            budget_data = self.budget_manager.collect_budget_data()
 
-        expected_data = {
-            "new_id": 123,
-            "client_id": 1,
-            "Legajo_vendedor": 456,
-            "Entrega_incluido": "Entrega incluida",
-            "Fecha_presupuesto": '2024-07-03',
-            "comentario": "Comentario",
-            "Condiciones": "50% Anticipo, 50% contra entrega",
-            "subtotal": 1000,
-            "tiempo_dias_valido": 30
-        }
-
-        self.assertEqual(budget_data, expected_data)
-
-    def test_select_salesperson(self):
-        vendedores_mock = [(1, 123, 'Juan', 'Perez'), (2, 456, 'Ana', 'Gomez')]
-        self.mock_cursor.fetchall.return_value = vendedores_mock
-
-        with patch('builtins.input', side_effect=['2']):
-            Legajo_vendedor = self.budget_manager.select_salesperson()
-
-        self.assertEqual(Legajo_vendedor, 456)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/test_main.py
@@ -0,0 +1,31 @@
+#Presupuestador/tests/test_main.py
+import unittest
+from unittest.mock import patch, MagicMock
+import os
+from src.main import PresupuestadorApp
+
+class TestPresupuestadorApp(unittest.TestCase):
+
+    @patch('os.system')
+    @patch('src.main.create_connection')
+    @patch('src.main.main_menu', side_effect=['0'])  # Aseguramos que el menú devuelve '0' para salir
+    @patch('src.main.UserInterface.mostrar_bienvenida')
+    @patch('src.main.LoggerConfigurator.get_logger')
+    def test_iniciar(self, mock_get_logger, mock_mostrar_bienvenida, mock_main_menu, mock_create_connection, mock_os_system):
+        mock_logger = MagicMock()
+        mock_get_logger.return_value = mock_logger
+        mock_conn = MagicMock()
+        mock_create_connection.return_value = mock_conn
+        
+        app = PresupuestadorApp()
+        app.iniciar()
+        
+        mock_os_system.assert_called_once_with('cls' if os.name == 'nt' else 'clear')
+        mock_create_connection.assert_called_once()
+        mock_mostrar_bienvenida.assert_called()
+        mock_main_menu.assert_called()
+        mock_logger.debug.assert_any_call("Iniciando la aplicación")
+        mock_logger.info.assert_any_call("Saliendo del programa")
+
+if __name__ == '__main__':
+    unittest.main()
