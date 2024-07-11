0a8661e49da2d8f23301839109bdbb1e8654fde9
AgustinMadygraf <agustin,mtto.madygraf@gmail.com>
2024-07-11 11:56:08 -0300

    Refactor file path in main.py and client_selection.py

--- run.py
+++ run.py
@@ -4,17 +4,10 @@ import os
 
 # Asegúrate de que el directorio `src` esté en el `PYTHONPATH`
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-input("Presione Enter para salir...")
 
 from src.main import PresupuestadorApp
-input("Presione Enter para salir...")
 
 if __name__ == "__main__":
-    input("Presione Enter para salir...")
-
     app = PresupuestadorApp()
-    input("Presione Enter para salir...")
-
     app.iniciar()
-    input("Presione Enter para salir...")
 
