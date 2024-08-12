612fa8e510b66dd5f5dec049faea15a3c943fda6
AgustinMadygraf <agustin.mtto.madygraf>
2024-08-11 21:46:16 -0300

    Refactor setup.py for improved code organization and readability

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-# Presupuestador/setup.py
+"""
+Setup script for Presupuestador.
+"""
+
 import subprocess
 import sys
 import os
-import glob
-from src.install.dependency_manager import PipUpdater, DependencyVerifier, PipDependencyInstaller, DependencyInstallerManager
+from src.install.dependency_manager import (
+    PipUpdater, DependencyVerifier, PipDependencyInstaller, DependencyInstallerManager
+)
 from src.install.installer_utils import is_pipenv_updated, list_python_interpreters
 
 if __name__ == "__main__":
@@ -21,19 +25,30 @@ if __name__ == "__main__":
     print("Intérpretes de Python encontrados:")
     for i, interpreter in enumerate(python_interpreters):
         print(f"[{i}] {interpreter}")
-    
+
     # Solicitar selección de intérprete de Python
-    selected_index = input("Selecciona el número del intérprete de Python a utilizar (o deja en blanco para usar el actual): ")
-    python_executable = python_interpreters[int(selected_index)] if selected_index else sys.executable
+    selected_index = input(
+        "Selecciona el número del intérprete de Python a utilizar "
+        "(o deja en blanco para usar el actual): "
+    )
+    PYTHON_EXECUTABLE = python_interpreters[int(selected_index)] 
+    if selected_index:
+        PYTHON_EXECUTABLE = python_interpreters[int(selected_index)]
+    else:
+        PYTHON_EXECUTABLE = sys.executable
 
     # Lista de dependencias que se requiere verificar e instalar
-    dependencies = ["pipenv", "winshell", "win32com.client", "pywintypes", "colorlog"]
-    
+    dependencies = [
+        "pipenv", "winshell", "win32com.client", "pywintypes", "colorlog"
+    ]
+
     # Crear instancias de las clases necesarias
     pip_updater = PipUpdater()
     verifier = DependencyVerifier(dependencies)
-    installer_manager = DependencyInstallerManager(PipDependencyInstaller(), pip_updater, max_retries=3)
-    
+    installer_manager = DependencyInstallerManager(
+        PipDependencyInstaller(), pip_updater, max_retries=3
+    )
+
     # Verifica e instala las dependencias faltantes
     missing_dependencies = verifier.get_missing_dependencies()
     if missing_dependencies:
@@ -41,11 +56,11 @@ if __name__ == "__main__":
         installer_manager.install_missing_dependencies(missing_dependencies)
     else:
         print("Todas las dependencias están instaladas.")
-    
+
     # Verifica si pipenv está actualizado
-    if not is_pipenv_updated(python_executable):
+    if not is_pipenv_updated(PYTHON_EXECUTABLE):
         print("Actualizando dependencias con pipenv...")
-        subprocess.check_call([python_executable, '-m', 'pipenv', 'install'])
+        subprocess.check_call([PYTHON_EXECUTABLE, '-m', 'pipenv', 'install'])
 
     try:
         # Intento de importar y ejecutar el instalador del proyecto
